cc73a56dd96cabbfdcebb5ca2a18e83fcac41208
Ben Hessel <hesselfitness@gmail.com>
2022-09-07 13:42:36 -0400

    switched to dev firebase BE, setting up auth first

--- src/components/App.js
+++ src/components/App.js
@@ -1,61 +1,28 @@
 import { useState, useEffect } from "react";
-import { db } from "../firebase-config";
-import { collection, getDocs, addDoc, updateDoc, doc, deleteDoc } from "firebase/firestore";
+import Signup from "./Signup"
+// import { db } from "../firebase-config";
+// import { collection, getDocs,  } from "firebase/firestore";
 
 function App() {
-  const [newName, setNewName] = useState("");
-  const [newAge, setNewAge] = useState(0);
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
+  // const [ users, setUsers ] = useState([])
+  // const usersCollectionRef = collection(db, "users");
 
-  const createUser = async () => {
-    await addDoc(usersCollectionRef, { name: newName, age: newAge });
-  };
 
-  const updateUser = async (id, age) => {
-    const userDoc = doc(db, "users", id)
-    const newFields = {age: age + 1}
-    await updateDoc(userDoc, newFields)
-  }
+  // useEffect(() => {
+  //   const getUsers = async () => {
+  //     const getUsersData = await getDocs(usersCollectionRef);
+  //     setUsers(getUsersData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  //   };
+  //   return () => {
+  //     getUsers();
+  //   };
+  // }, [usersCollectionRef]);
 
-  const deleteUser = async (id) => {
-    const userDoc = doc(db, "users", id)
-    await deleteDoc(userDoc)
-  }
-
-  useEffect(() => {
-    const getUsers = async () => {
-      const getUsersData = await getDocs(usersCollectionRef);
-      setUsers(getUsersData.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-    return () => {
-      getUsers();
-    };
-  }, [usersCollectionRef]);
 
   return (
     <div className="App">
-      <input
-        placeholder="Name..."
-        onChange={(e) => setNewName(e.target.value)}
-      />
-      <input
-        type="number"
-        placeholder="Age..."
-        onChange={(e) => setNewAge(e.target.value)}
-      />
-      <button onClick={createUser}>Create User</button>
-      <h1>Hello World</h1>
-      {users.map((user, i) => (
-        <div key={i}>
-          <h2>Name: {user.name}</h2>
-          <h2>Age: {user.age}</h2>
-          <button onClick={() => updateUser(user.id, user.age)}>Increase Age</button>
-          <button onClick={() => deleteUser(user.id)}>Delete Account</button>
-        
-        </div>
-
-      ))}
+      <h1>hello world</h1>
+      <Signup />
     </div>
   );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,13 +1,20 @@
 //this page will be for users to sign up and access their logged-in home (private routes) for order history + to make profile changes
 
-import React from 'react';
+import React, { useRef } from "react";
 
 const Signup = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+//   const emailRef = useRef();
+
+  return (
+    <>
+      <div className="card">
+      <h2>Signup page</h2>
+        <form onSubmit="">
+          
+        </form>
+      </div>
+    </>
+  );
+};
 
 export default Signup;
