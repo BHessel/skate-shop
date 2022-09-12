0f031c602defe28d7a0273dc352a191d4e53d65d
Ben Hessel <hesselfitness@gmail.com>
2022-09-12 17:04:58 -0400

    logo up links to home, menu items listed, cart SVG up, do flexbox/structure/some basic design next

---
+++ src/components/NavItems.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const NavItems = (props) => {
+  const { items } = props;
+
+  return (
+    //I could change these links to a button w/ link. Might be easier for styling?
+    <>
+      <Link to={items.route}>
+        <li>{items.title}</li>
+      </Link>
+    </>
+  );
+};
+
+export default NavItems;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,42 @@
 import React from "react";
-
+import logo from "../images/localSkateShopLogo.jpeg";
+import NavItems from "./NavItems";
+import { Link } from "react-router-dom";
+import cartSVG from "../images/shoppingCartSVG.svg";
 //left side will have logo
 //middle will have ul/li list
 //right corner is createAccount/signin link plus cart
 
 const Navbar = () => {
+  const navBarItems = [
+    { title: "Skateboards", route: "/skateboards" },
+    { title: "Shoes", route: "/shoes" },
+    { title: "Accessories", route: "/accessories" },
+    { title: "Local", route: "/local" },
+  ];
+
   return (
-    <>
+    <nav>
       <div>
-        <h2>Logo Placeholder</h2>
+        <Link to="/">
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
       <div>
-        <ul> {/* ul should be menuItems.map.... LI's should actually be on a net component */}
-          <li>Skateboards</li> {/*decks, trucks, etc*/}
-          <li>Shoes</li> {/*shoes*/}
-          <li>Accessories</li> {/*wax, griptape, etc*/}
-          <li>Local</li> {/*team, events, etc*/}
+        <ul>
+          {navBarItems.map((item, index) => {
+            return <NavItems items={item} key={index} />;
+          })}
         </ul>
       </div>
-      <div>Create Account</div> {/*conditionally render Welcome back, name! (if logged in)*/}
       <div>
-        {/* {cartImage} */}
+        <Link to="signup">Create Account</Link>
       </div>
-    </>
+      {/*conditionally render Welcome back, name! (if logged in)*/}
+      <div>
+        <img src={cartSVG} alt="cartSVG"/>
+      </div>
+    </nav>
   );
 };
 
