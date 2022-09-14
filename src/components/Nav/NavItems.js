992361c9f6f277aa1755ec0f21bdc045df4253ea
Ben Hessel <hesselfitness@gmail.com>
2022-09-14 13:40:27 -0400

    banner MVP done, improve w/ styling @ end

--- src/components/Nav/NavItems.js
+++ src/components/Nav/NavItems.js
@@ -1,35 +1,71 @@
-// import { useState } from "react";
+import { Fragment } from "react";
 // import { Link } from "react-router-dom";
-// import { Menu } from "@headlessui/react";
-// import { ChevronDownIcon } from "@heroicons/react/solid";
+import { Menu, Transition } from "@headlessui/react";
+import { ChevronDownIcon } from "@heroicons/react/20/solid";
 
-// const NavItems = (props) => {
-//   const { items } = props;
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
-//   // console.log(items)
-//   return (
-    
-//     // <>
-//     <li>
-//       {items.dropdown ? (
-//         <>
-//           <Link to={items.route}>
-//             <button
-//               className="px-4 border border-red-800"
-//               type="button"
-//             >
-//               {items.title}
-//             </button>
-//           </Link>
-//         </>
-//       ) : (
-//         <Link to={items.route}>
-//           <button className="px-4">{items.title}</button>
-//         </Link>
-//       )}
-//     </li>
-//     // </>
-//   );
-// };
+const conditionalNavMovement = () => {
+  //if menu.item is to the left of the screen, return classname  w left-0
+  //else return classname w right-0
+  //CAN BE DONE HARD CODED if needed for MVP
+  //could also use a ternary... if menu.items screen location is left return classname w left-0, etc.
+}
 
-// export default NavItems;
+const NavItems = (props) => {
+  const { item } = props;
+
+  return (
+    <li className="p-4">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100">
+            {item.title}
+            <ChevronDownIcon
+              className="-mr-1 ml-2 h-5 w-5"
+              aria-hidden="true"
+            />
+          </Menu.Button>
+        </div>
+
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              {item.dropdown.map((subMenu, index) => {
+                return (
+                  <Menu.Item>
+                    {({ active }) => (
+                      <a
+                        href={subMenu.route}
+                        className={classNames(
+                          active
+                            ? "bg-gray-100 text-gray-900"
+                            : "text-gray-700",
+                          "block px-4 py-2 text-sm"
+                        )}
+                      >
+                        {subMenu.title}
+                      </a>
+                    )}
+                  </Menu.Item>
+                );
+              })}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  );
+};
+
+export default NavItems;
