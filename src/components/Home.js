9fd611c2f9b93096e07e72d5a93be96312d16d08
Ben Hessel <hesselfitness@gmail.com>
2022-09-18 16:53:03 -0400

    break

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ import Newsletter from "./Newsletter";
 import Maps from "./Maps/Maps";
 import Footer from "./Footer";
 
-const location = {
-  address: "17010 S 48th St, Phoenix, AZ 85048",
-  lat: 33.29142,
-  lng: -111.97979,
-};
+// const location = {
+//   address: "17010 S 48th St, Phoenix, AZ 85048",
+//   lat: 33.29142,
+//   lng: -111.97979,
+// };
 
 const Home = () => {
   return (
@@ -32,7 +32,7 @@ const Home = () => {
         <Newsletter />
       </div>
       <div className="flex justify-center mt-10">
-        <Maps location={location} center={{lat: 33.29142, long: -111.97979}} zoomLevel={17} />
+        <Maps />
       </div>
       <div className="flex flex-row justify-around mt-10 w-full h-80 bg-black bg-opacity-90 p-5">
         <Footer />
--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,28 +1,11 @@
-import React from "react";
-import GoogleMapReact from "google-map-react";
-import { Icon } from "@iconify/react";
-// import locationIcon from "@iconify/icons-mdi/map-marker";
+import React from 'react';
 
-const Maps = ({ location, zoomLevel }) => {
-  const LocationPin = ({ text }) => (
-    <div className="pin">
-      <Icon icon="mdi:map-marker" className="pin-icon" />
-      <p className="pin-text">{text}</p>
-    </div>
-  );
-
-//   console.log(location)
-  return (
-    <div className="w-full h-screen">
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: process.env.REACT_APP_MAPS_KEY }}
-        defaultCenter={location}
-        defaultZoom={zoomLevel}
-      >
-        <LocationPin lat={location.lat} lng={location.lng} text="The Shop" />
-      </GoogleMapReact>
-    </div>
-  );
-};
+const Maps = () => {
+    return (
+        <div>
+            <h1>maps here</h1>
+        </div>
+    );
+}
 
 export default Maps;
