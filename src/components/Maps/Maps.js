e33b0808da715d82f9bef1cd663d6117e7629f46
Ben Hessel <hesselfitness@gmail.com>
2022-09-19 17:38:15 -0400

    google map is up, marker is up, may have issues w/ marker on re-render?

--- src/components/Maps/Maps.js
+++ src/components/Maps/Maps.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
 const Maps = () => {
-    return (
-        <div>
-            <h1>maps here</h1>
-        </div>
-    );
-}
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_MAPS_KEY,
+  });
+
+  const center = useMemo(() => ({ lat: 33.29142, lng: -111.97979 }), []);
+
+  return !isLoaded ? (
+    <div>Loading Map to Shop...</div>
+  ) : (
+    <GoogleMap
+      zoom={10}
+      center={center}
+      mapContainerClassName="w-[600px] h-[400px]"
+    >
+      <>
+        <Marker position={center} />
+      </>
+    </GoogleMap>
+  );
+};
 
 export default Maps;
